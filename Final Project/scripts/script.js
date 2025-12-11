d4379becfa5d61743674892aca37344be2c62234
davidhv123 <davidhv123321@gmail.com>
2025-12-11 00:20:20 -0500

    version6

--- Final Project/scripts/script.js
+++ Final Project/scripts/script.js
@@ -157,3 +157,47 @@ loadWeather();
     console.error('Error al cargar el JSON:', err);
   }
 })();
+
+// ------------------------------------------------------
+// ---------------------- PROJECTS PAGE --------------------
+// ------------------------------------------------------
+import { projects } from "../data/projects.mjs"
+console.log(projects)
+
+const showhere = document.getElementById("my-projects")
+
+function displayItems(projects) {
+  projects.forEach(x => {
+
+    // build the card element
+    const thecard = document.createElement("div")
+
+    // build the photo element
+    const thephoto = document.createElement("img")
+    thephoto.src = `images/${x.photo_link}`
+    thephoto.alt = x.name
+    thecard.appendChild(thephoto)
+
+    // build the title element
+    const thetitle = document.createElement("h2")
+    thetitle.innerText = x.name
+    thecard.appendChild(thetitle)
+
+    // build the address element
+    const theaddress = document.createElement("address")
+    theaddress.innerText = x.address
+    thecard.appendChild(theaddress)
+
+    // build the description element
+    const thedesc = document.createElement("p")
+    thedesc.innerText = x.description
+    thecard.appendChild(thedesc)
+
+
+    showhere.appendChild(thecard)
+  })
+}
+
+// ------------------------------------------------------
+// ---------------------- CONTACT PAGE --------------------
+// ------------------------------------------------------
